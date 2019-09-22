72e81782598c3625c47ee10d24583564a75d733c
sugar3103 <duong16628@gmail.com>
2019-09-22 16:35:09 +0700

    initial project push

---
+++ src/Component/Test/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Test() {
+  if (!Function.prototype.bind) {
+    Function.prototype.bind = function(oThis) {
+      if (typeof this !== "function") {
+        throw new TypeError(
+          "Function.prototype.bind - what is trying to be bound is not callable"
+        );
+      }
+      var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function() {},
+        fBound = function() {
+          return fToBind.apply(
+            this instanceof fNOP ? this : oThis,
+            aArgs.concat(Array.prototype.slice.call(arguments))
+          );
+        };
+      if (this.prototype) {
+        fNOP.prototype = this.prototype;
+      }
+      fBound.prototype = new fNOP();
+      return fBound;
+    };
+  }
+  return <div></div>;
+}
+
+export default Test;
